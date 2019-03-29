4882baac288ea237b7af7416fbb125dc29859a7a
Zijian Shao <sam.zj.shao@gmail.com>
2019-03-29 16:44:31 -0400

    Chrome V2.1.4

--- Learn_DarkLight_Chrome/theme/theme_1/functions.js
+++ Learn_DarkLight_Chrome/theme/theme_1/functions.js
@@ -2,27 +2,35 @@ function initTheme() {
 
     var cssText = '';
     if (!getCustomThemeOption('whiteNavbar')) {
-        cssText += '.d2l-navigation-s-main, d2l-navigation-bottom-bar, d2l-navigation-main-footer' +
+        // black bar
+        cssText += 'd2l-navigation-main-footer' +
             '{border:none!important;background:rgba(0, 0, 0, 0.8)!important;box-shadow:0 2px 4px rgba(0, 0, 0, 0.2)!important;}';
-        // cssText += '.d2l-navigation-s-shadow-gradient, .d2l-navigation-shadow-gradient' +
-        //     '{background:none!important;}';
         cssText += 'd2l-navigation-main-footer a.d2l-navigation-s-link, d2l-navigation-main-footera.d2l-navigation-s-link:link, d2l-navigation-main-footera.d2l-navigation-s-link:visited' +
             '{color:#aaa!important;}';
-        cssText += 'd2l-navigation-main-footer .d2l-navigation-s-item .d2l-navigation-s-group-text, d2l-navigation-main-footer .d2l-navigation-s-item .d2l-icon-0' +
+        cssText += 'd2l-navigation-main-footer .d2l-navigation-s-item .d2l-navigation-s-group-text, d2l-navigation-main-footer .d2l-navigation-s-item d2l-icon' +
             '{color:#aaa!important;}';
     } else {
-        cssText += '.d2l-navigation-s-main, d2l-navigation-bottom-bar, d2l-navigation-main-footer' +
+        // white bar
+        cssText += 'd2l-navigation-main-footer' +
             '{box-shadow:0 2px 3px rgba(0, 0, 0, 0.02)!important;}' +
             '.d2l-page-main:before{content:"";background:linear-gradient(to bottom, rgba(249,250,251,1) 0%,rgba(249,250,251,0) 100%);' +
             'position:absolute;top:0;left:0;right:0;height:50vh;max-height:400px;z-index:-10;}';
     }
-    if (getCustomThemeOption('fullWidthLayout')) {
-        body.addClass('darklight-fullwidth');
-    }
+
     chrome.runtime.sendMessage({
         action: 'insertCSS',
         data: {code: cssText}
     });
+
+    injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_navbar.css',
+        $('d2l-navigation'), 'file', true);
+    injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_widget_header_dropdown.css',
+        $('.d2l-homepage-header-menu-wrapper d2l-dropdown'), 'file', true);
+
+    if (getCustomThemeOption('fullWidthLayout')) {
+        body.addClass('darklight-fullwidth');
+        injectCSSShadow('div.d2l-navigation-centerer {max-width: none !important}', $('d2l-navigation'), 'text', true);
+    }
 
 }
 
