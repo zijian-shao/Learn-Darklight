a11a97d795d56acc42194cde7a2c14cafb2ca15a
Zijian Shao <sam.zj.shao@gmail.com>
2019-04-01 12:52:46 -0400

    Chrome V2.1.7

--- Learn_DarkLight_Chrome/theme/theme_3/functions.js
+++ Learn_DarkLight_Chrome/theme/theme_3/functions.js
@@ -14,8 +14,29 @@ function initTheme() {
 
     $('d2l-navigation').append('<div class="blue-banner"></div>');
 
-    injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_navbar.css',
-        $('d2l-navigation'), 'file', true);
+    var d2lNavigation = document.getElementsByTagName('d2l-navigation');
+    if (d2lNavigation.length) {
+        d2lNavigation = d2lNavigation[0];
+        if (d2lNavigation.shadowRoot !== null) {
+            d2lNavigation.setAttribute('data-navbar-init', '');
+            injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_navbar.css',
+                $(d2lNavigation), 'file', true);
+        } else {
+            var navCounter = 0;
+            var navInterval = setInterval(function () {
+                if (d2lNavigation.shadowRoot !== null || navCounter > 20) {
+                    clearInterval(navInterval);
+                    d2lNavigation.setAttribute('data-navbar-init', '');
+                    setTimeout(function () {
+                        injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_navbar.css',
+                            $(d2lNavigation), 'file', true);
+                    }, 10);
+                }
+                navCounter++;
+            }, 200);
+        }
+    }
+
     injectCSSShadow(baseURL + 'theme/theme_' + options.GLB_ThemeID + '/shadow_widget_header_dropdown.css',
         $('.d2l-homepage-header-menu-wrapper d2l-dropdown'), 'file', true);
 
