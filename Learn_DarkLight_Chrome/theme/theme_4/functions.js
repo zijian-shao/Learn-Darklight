5850c52e364c1416abaa6d3e0345fa747d848450
Zijian Shao <sam.zj.shao@gmail.com>
2019-05-03 01:05:28 -0400

    Chrome V3.0.0

--- Learn_DarkLight_Chrome/theme/theme_4/functions.js
+++ Learn_DarkLight_Chrome/theme/theme_4/functions.js
@@ -4,7 +4,7 @@ function initTheme() {
     var colorScheme = getCustomThemeOption('colorScheme');
     var colorItem = ['--dlight-band-color-1', '--dlight-band-color-2', '--dlight-band-color-3', '--dlight-band-color-4', '--dlight-nav-header-highlight-color', '--dlight-menu-hover-text-color', '--dlight-menu-hover-border-color', '--dlight-menu-hover-background-color', '--dlight-menu-hover-background-color-plus', '--dlight-widget-header-background-color', '--dlight-widget-header-text-color', '--dlight-link-hover-color', '--dlight-btn-primary-background-color', '--dlight-btn-primary-border-color', '--dlight-btn-primary-text-color', '--dlight-btn-primary-hover-background-color', '--dlight-btn-primary-hover-border-color', '--dlight-btn-primary-hover-text-color', '--dlight-btn-primary-hover-box-shadow-color', '--dlight-btn-default-background-color', '--dlight-btn-default-border-color', '--dlight-btn-default-text-color', '--dlight-btn-default-hover-background-color', '--dlight-btn-default-hover-border-color', '--dlight-btn-default-hover-text-color', '--dlight-btn-default-hover-box-shadow-color'];
     var colorData = {
-        default: ['#fffaaa', '#ffea30', '#fdd54f', '#e4b429', '#fdd54f', '#222', '#999', '#eee', '#f6f6f6', '#000000', '#fdd54f', '#f4ad24', '#333333', '#333333', '#fdd54f', '#fdd54f', '#fdd54f', '#333333', 'rgba(0,0,0,0.7)', '#f9fafb', '#d3d9e3', '#565a5c', '#333333', '#333333', '#ffffff', 'rgba(0,0,0,0.2)'],
+        default: ['#fffaaa', '#ffea30', '#fdd54f', '#e4b429', '#fdd54f', '#222222', '#999999', '#eeeeee', '#f6f6f6', '#222222', '#fdd54f', '#f4ad24', '#333333', '#333333', '#fdd54f', '#fdd54f', '#fdd54f', '#333333', 'rgba(0,0,0,0.7)', '#f9fafb', '#d3d9e3', '#565a5c', '#333333', '#333333', '#ffffff', 'rgba(0,0,0,0.2)'],
         ahs: ['#97dfef', '#00bed0', '#0098a5', '#005963', '#00bed0', '#0098a5', '#00bed0', '#d5f2f9', 'rgba(213,242,249,.7)', '#0098a5', '#ffffff', '#0098a5', '#0098a5', '#0098a5', '#ffffff', '#97dfef', '#97dfef', '#005963', '#0098a5', '#f9fafb', '#d3d9e3', '#565a5c', '#0098a5', '#0098a5', '#ffffff', '#97dfef'],
         art: ['#ffd5a5', '#fbaf00', '#e78100', '#d93f00', '#fbaf00', '#d93f00', '#e78100', '#ffeedb', 'rgba(255,238,219,.6)', '#d93f00', '#ffffff', '#d93f00', '#d93f00', '#d93f00', '#ffffff', '#ffd5a5', '#ffd5a5', '#d93f00', '#e78100', '#f9fafb', '#d3d9e3', '#565a5c', '#d93f00', '#d93f00', '#ffffff', '#ffd5a5'],
         eng: ['#d0b4ef', '#be33da', '#8100b4', '#57058b', '#d0b4ef', '#8100b4', '#8100b4', '#ece1f9', 'rgba(236,225,249,.7)', '#57058b', '#d0b4ef', '#57058b', '#57058b', '#57058b', '#ffffff', '#d0b4ef', '#d0b4ef', '#57058b', '#8100b4', '#f9fafb', '#d3d9e3', '#565a5c', '#57058b', '#57058b', '#ffffff', '#d0b4ef'],
@@ -27,6 +27,14 @@ function initTheme() {
     // full width
     if (getCustomThemeOption('fullWidthLayout')) {
         body.addClass('darklight-fullwidth');
+    }
+
+    // tile mode for widget
+    if (!getCustomThemeOption('tileModeForWidgets')) {
+        body.addClass('darklight-tile-legacy');
+        $('.d2l-widget').removeClass('d2l-tile');
+        // $('.d2l-widget .vui-heading-4').removeClass('vui-heading-4').addClass('vui-heading-2');
+        $('.d2l-body').removeClass('daylight-widget-demarcation d2l-tiles-container');
     }
 
     // wait for navbar
@@ -119,6 +127,15 @@ function themeOnCourseTileLoaded(elem) {
             '}',
             $(elem.shadowRoot), 'text');
     }
+}
+
+function themeOnCourseTabAvailable(d2lTabs) {
+    d2lTabs.shadowRoot.querySelectorAll(':host > .d2l-tabs-layout > .d2l-tabs-container > .d2l-tabs-container-list > d2l-tab').forEach(function (el) {
+        if (el.shadowRoot !== null) {
+            injectCSS(':host([aria-selected="true"]:focus) .d2l-tab-selected-indicator{box-shadow:0 0 0 3px var(--dlight-btn-default-hover-box-shadow-color)}',
+                $(el.shadowRoot), 'text');
+        }
+    });
 }
 
 initTheme();
